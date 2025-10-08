e0c8958575d34513ff9ae3dad393fe8964f526e0
SHRUTIGUNGE <shrutigunge5@gmail.com>
2025-10-08 11:31:28 +0530

    new file added

--- tests/practice.e.js
+++ tests/practice.e.js
@@ -1,8 +1,17 @@
-// function add(a,b){
-//     return a+b
-// }
-// console.log(add(10,20));
+// // function add(a,b){
+// //     return a+b
+// // }
+// // console.log(add(10,20));
 
-let a = 3, b = 7;
-[a, b] = [b, a];
-console.log(a, b);
+// addE(20,30)
+let evennumbers=[]
+for(let i=0;i<=60;i++){
+    if(i%2===0){
+        evennumbers.push(i)
+    }
+
+    let first10=evennumbers.slice(0,10)
+    let next10=evennumbers.slice(10,20)
+    const result=[...first10,...next10]
+}
+console.log(result);
